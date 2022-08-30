75fbc53d64b45c65c8b31287df12a40228a59ef3
Miguel Torres <themtorres29@yahoo.com>
2022-08-30 11:58:51 -0700

    Added crew battle video

--- src/components/SmashInfo.js
+++ src/components/SmashInfo.js
@@ -21,6 +21,13 @@ const SmashInfo = () => {
               </div>
             </div>
 
+            <div className="smash-video-container">
+              <h3>JV3 2022 Crew battle Los Lecheros vs SuperJellyNovaGang</h3>
+              <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/dIGwlR1oERA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </div>
+            </div>
+
             <h3 className="smash-coming-soon">more coming soon</h3>
         </div>
       </div>
